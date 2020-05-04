872339811c76a1fff9ac48dbf66498834d209717
anhtudo97 <anhtudo97@gmail.com>
2020-05-05 00:55:48 +0700

    feat: init detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/widgets/search_bar.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({Key key}) : super(key: key);
@@ -17,6 +18,48 @@ class DetailScreen extends StatelessWidget {
               image: DecorationImage(
                 image: AssetImage('assets/images/meditation_bg.png'),
                 fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: size.height * .05,
+                  ),
+                  Text(
+                    'Meditation',
+                    style: Theme.of(context)
+                        .textTheme
+                        .display1
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '3-10 MIN Course',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: size.width * .6,
+                    child: Text(
+                      "Live happier and healthier by learning the fundamentals of meditation and mindfulness",
+                    ),
+                  ),
+                  SizedBox(
+                    width: size.width * .5,
+                    child: SearchBar(),
+                  ),
+                ],
               ),
             ),
           ),
